8ede029bdb7ad213ed29a79eae8297f1ba57acd5
IxAntho <isabar735@icloud.com>
2024-02-03 02:02:07 -0500

    complete project

--- index.js
+++ index.js
@@ -11,45 +11,63 @@ for (let i = 0; i < buttons.length; i++) {
     // to get the Identity of the button that got clicked use "this"
     //Using switch statement to play a different sound depending on button
     const buttonText = this.textContent;
-    switch (buttonText) {
-      case "w":
-        audio = new Audio("./sounds/crash.mp3");
-        audio.play();
-        break;
-
-      case "a":
-        audio = new Audio("./sounds/kick-bass.mp3");
-        audio.play();
-        break;
-
-      case "s":
-        audio = new Audio("./sounds/snare.mp3");
-        audio.play();
-        break;
-
-      case "d":
-        audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
-        break;
-
-      case "j":
-        audio = new Audio("./sounds/tom-2.mp3");
-        audio.play();
-        break;
-
-      case "k":
-        audio = new Audio("./sounds/tom-3.mp3");
-        audio.play();
-        break;
-
-      case "l":
-        audio = new Audio("./sounds/tom-4.mp3");
-        audio.play();
-        break;
-
-      default:
-        HTMLFormControlsCollection.log(buttonText);
-        break;
-    }
+    makeSound(buttonText);
+    buttonAnimation(buttonText);
   });
+}
+
+document.addEventListener("keydown", function (evt) {
+  makeSound(evt.key);
+  buttonAnimation(evt.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      audio = new Audio("./sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+
+    case "d":
+      audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    default:
+      HTMLFormControlsCollection.log(buttonText);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector(`.${currentKey}`);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
